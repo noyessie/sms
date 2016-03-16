43a05ddda68145e82160db1258121712e3ee0d86
hubert <hubertnoyessie@gmail.com>
2016-03-16 19:01:27 +0100

    pdo factory conflict

--- Library/PDOFactory.php
+++ Library/PDOFactory.php
@@ -4,11 +4,7 @@ class PDOFactory
 {
 	public static function getMysqlConnexion()
 	{
-<<<<<<< HEAD
-		$db = new \PDO('mysql:dbname=mydb','dev','');
-=======
 		$db = new \PDO('mysql:dbname=sms','root','');
->>>>>>> 570b1b1806f824467591b10f3b147f76c37b409b
 		$db->setAttribute(\PDO::ATTR_ERRMODE , \PDO::ERRMODE_EXCEPTION);
 		return $db;
 	}
