faa10ed0053d48062b186df4bbb1bc7e9418a125
hubert <hubertnoyessie@gmail.com>
2016-03-16 10:32:42 +0100

    sms et user manager

--- Library/Models/SMSManager_PDO.php
+++ Library/Models/SMSManager_PDO.php
@@ -3,32 +3,14 @@ namespace Library\Models;
 use Library\Manager;
 use Library\Entities\SMS;
 
-class SMSManager_PDO extends Manager{
-	public function create(SMS $sms){
-
-	}
-
-	public function modify(SMS $sms){
-
-	}
-
-	public function delete(SMS $Sms){
-
-	}
-
-	public function find($data = array()){
-
-	}
-
-	public function get($id=0){
-		if(!is_numeric($id)){
-			return new SMS();
-		}
-		$result = $this->find(array('id'=>$id));
-		if(count($result)>0){
-			return $result[0];
-		}else{
-			return new SMS();
-		}
+class SMSManager_PDO extends ManagerCRUD{
+	public function __construct(){
+		$this->mapping = array(
+				'id'=>'idsms',
+				'corps'=>'corps',
+				'dateEnvoie'=>'dateEnvoie',
+				'dateEnregistrement'=>'dateEnregistrement',
+				'idUser'=>'user_iduser',
+			)
 	}
 }
--- Library/Models/UserManager_PDO.php
+++ Library/Models/UserManager_PDO.php
@@ -3,32 +3,19 @@ namespace Library\Models;
 use Library\Manager;
 use Library\Entities\User;
 
-class UserManager_PDO extends Manager{
-	public function create(User $user){
+class UserManager_PDO extends ManagerCRUD{
 
-	}
+	
 
-	public function modify(User $user){
-
-	}
-
-	public function delete(User $user){
-
-	}
-
-	public function find($data = array()){
-
-	}
-
-	public function get($id=0){
-		if(!is_numeric($id)){
-			return new User();
-		}
-		$result = $this->find(array('id'=>$id));
-		if(count($result)>0){
-			return $result[0];
-		}else{
-			return new User();
-		}
+	public function __construct(){
+		parent::construct();
+		$this->mapping = array(
+			'id'=>'iduser',
+			'nom'=>'nom',
+			'prenom'=>'prenom',
+			'email'=>'adressemail',
+			'password'=>'motdepasse',
+		);
+		$this->table_name = "User";
 	}
 }
