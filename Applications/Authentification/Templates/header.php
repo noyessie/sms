88fa28b1ee8e857575a274a8f54481e1c6a50343
patricksanang <patrick.sanang@gmail.com>
2016-03-24 16:16:21 +0100

    ajout des fichier au depots

---
+++ Applications/Authentification/Templates/header.php
@@ -0,0 +1,14 @@
+<!-- balises meta-->
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device, initial-scale=1.0"/> <!-- suivant le device, le navigateur doit lire le fichier css -->
+
+        <!-- titre--> 
+        <title>Login</title>
+
+        <link rel="stylesheet" href="/sms/Web/bootstrap/css/bootstrap.css">
+         <link rel="stylesheet" href="/sms/Web/css/style.css">
+
+        <script src="/sms/Web/js/jquery-1.10.2.min.js"></script>
+        <script src="/sms/Web/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/sms/Web/bootstrap/js/bootstrap-dropdown.js"></script>
+       
