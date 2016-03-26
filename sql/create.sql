b3d4f3a4995db7af5c990795224137078e3d511e
patricksanang <patrick.sanang@gmail.com>
2016-03-26 08:23:32 +0100

    'save_4'

--- sql/create.sql
+++ sql/create.sql
@@ -57,14 +57,30 @@ CREATE TABLE IF NOT EXISTS contact_has_groupe (
   CONSTRAINT fk_contactHasGroupe_groupeIdgroupe_groupe FOREIGN KEY (groupe_idgroupe) REFERENCES groupe(idgroupe) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS sms_has_contact(
-  id_sms_has_contact INT AUTO_INCREMENT,
-  sms_idsms INT NOT NULL,
-  contact_idcontact INT NOT NULL,
-  etat VARCHAR(10),
-  dateEnvoie TIMESTAMP,
-  CONSTRAINT pk_sms_has_contact PRIMARY KEY(id_sms_has_contact),
-  CONSTRAINT fk_smsHasContact_smsIdsms_sms FOREIGN KEY (sms_idsms) REFERENCES sms(idsms) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_smsHasContact_contactIdcontact_contact FOREIGN KEY (contact_idcontact) REFERENCES contact(idcontact) ON DELETE CASCADE ON UPDATE CASCADE
+-- --------------------------------------------------------
 
-);
+--
+-- Structure de la table `sms_has_contact`
+--
+
+CREATE TABLE IF NOT EXISTS `sms_has_contact` (
+  `id_sms_has_contact` int(11) NOT NULL AUTO_INCREMENT,
+  `sms_idsms` int(11) NOT NULL,
+  `contact_idcontact` int(11) NOT NULL,
+  `etat` varchar(10) DEFAULT NULL,
+  `dateEnvoie` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_sms_has_contact`),
+  KEY `fk_smsHasContact_smsIdsms_sms` (`sms_idsms`),
+  KEY `fk_smsHasContact_contactIdcontact_contact` (`contact_idcontact`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `sms_has_contact`
+--
+ALTER TABLE `sms_has_contact`
+  ADD CONSTRAINT `fk_smsHasContact_contactIdcontact_contact` FOREIGN KEY (`contact_idcontact`) REFERENCES `contact` (`idcontact`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_smsHasContact_smsIdsms_sms` FOREIGN KEY (`sms_idsms`) REFERENCES `sms` (`idsms`) ON DELETE CASCADE ON UPDATE CASCADE;
